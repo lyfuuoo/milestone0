69e41725d3f5701098a65031b735cb79f7938e16
ruyun <uu.ru@mail.utoronto.ca>
2021-01-17 10:20:37 -0500

    Modified main.cpp by computing a new center of mass with a new polynomial.

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -18,15 +18,17 @@ int main() {
     
     //define a density polynomial
     Polynomial *my_density_function = new Polynomial(7,3,0.5,2,1,5);
+    Polynomial *function=new Polynomial(1,1,2,2,3,3);
 
     //find the center of mass of a cuboid of size (5,6,10) with our density function
     Coordinates center_of_mass = compute_center_of_mass(*my_density_function,5,6,10);
+    Coordinates center = compute_center_of_mass(*function,5,5,5);
 
     //print out center of mass
     cout << setprecision(4) << "center of mass (x,y,z) = " << center_of_mass << endl;
+    cout << setprecision(4) << "center (x,y,z) = " << center << endl;
 
-    //a useless print statement
-    cout << "Program Finished!" << endl;
+
     
     return 0;
 }
